953c66be3a8143e7897b7ae6696c0c5ef7c023c7
hoge-piyo-256 <126233149+hoge-piyo-256@users.noreply.github.com>
2024-07-22 22:01:36 +0900

    ダイアログを実装

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const fileElem = document.getElementById('fileInput');
 const quizImageElem = document.getElementById('quizImage');
-const changeQuizButton = document.getElementById('changeQuizButton');
+const changeQuizButtonElem = document.getElementById('changeQuizButton');
+const resetAlreadyButtonElem = document.getElementById('resetAlreadyButton');
+const imagesAreaElem = document.getElementById('imagesArea');
+const closeButtonElem = document.getElementById('closeButton');
+const dialogElem = document.getElementById('dialog');
+const menuButtonElem = document.getElementById('menuButton');
+const notShowAlreadyElem = document.getElementById('notShowAlready');
 
-changeQuizButton.onclick = changeQuiz;
+const beginBarButtonElem = document.getElementById('beginBarButton');
+const endBarButtonElem = document.getElementById('endBarButton');
+
+beginBarButtonElem.onclick = toggleShowAll;
+endBarButtonElem.onclick = toggleShowAll;
+changeQuizButtonElem.onclick = changeQuiz;
 
 // Quizzes のデータ型
 // Index1: 画像のBlobURL String型 
 // Index2: 出題済みか Boolean型
 let quizzes = [];
+let showAll = false;
 
 fileElem.onchange = (e) => {
     let files = [...fileElem.files];
@@ -19,6 +31,7 @@ fileElem.onchange = (e) => {
     if(files.length === 0) {
         alert('少なくとも一件画像ファイルを指定してください。');
         quizzes = [];
+        createImageGrid();
         return;
     }
 
@@ -41,16 +54,107 @@ fileElem.onchange = (e) => {
     quizzes = files.map((file) => {
         return [URL.createObjectURL(file), false];
     });
+
+    createImageGrid();
 }
 
-function changeQuiz(excludeAlready = false) {
-    let quizSelection = excludeAlready ? quizzes.filter((quiz) => {return !quiz[1]}) : quiz;
+menuButtonElem.onclick = (e) => {
+    dialogElem.classList.toggle('invisible');
+    createImageGrid();
+}
+
+closeButtonElem.onclick = (e) => {
+    dialogElem.classList.toggle('invisible');
+}
+
+dialogElem.onclick = (e) => {
+    if(e.target == dialogElem) dialogElem.classList.toggle('invisible');
+}
+
+resetAlreadyButtonElem.onclick = (e) => {
+    const confirmText = '出題済をリセットし、既に表示した画像がまた表示されるようになります。\r\nこの操作は取り消せません。\r\n本当にリセットしますか?';
+    if(confirm(confirmText)) {
+        quizzes = quizzes.map((quiz) => {
+            return [quiz[0], false];
+        });
+        createImageGrid();
+    }
+}
+
+function changeQuiz() {
+    excludeAlready = notShowAlreadyElem.checked;
+    let quizSelection = excludeAlready ? quizzes.filter((quiz) => {return !quiz[1]}) : quizzes;
     if(quizSelection.length == 0) {
         alert('出題するクイズがありません');
         return;
     }
-    let blobUrl = quizSelection[random(0,quizSelection.length - 1)][0];
+    let index = random(0,quizSelection.length - 1);
+    quizSelection[index][1] = true;;
+    let blobUrl = quizSelection[index][0];
     quizImageElem.src =  blobUrl;
+}
+
+function createImageGrid() {
+    // 横方向にいくつ並ぶか
+    const gridColumn = 3;
+
+    // Border の色
+    const borderColor = 'var(--var-second-color)';
+
+    // Border の太さ
+    const borderSize = '2px';
+
+    if(quizzes.length > 9) {
+        beginBarButtonElem.classList.remove('hidden');
+        endBarButtonElem.classList.remove('hidden');
+    } else {
+        beginBarButtonElem.classList.add('hidden');
+        endBarButtonElem.classList.add('hidden');
+    }
+    
+    beginBarButtonElem.innerText = showAll ? '一部のみ表示' : 'すべて表示';
+    endBarButtonElem.innerText = showAll ? '一部のみ表示' : 'すべて表示';
+
+    let filteredQuizzes = showAll ? quizzes : quizzes.slice(0,9);
+
+    // セル1つの横幅
+    let gridWidth = imagesAreaElem.clientWidth / gridColumn;
+
+    // Grid の親要素(コンテナ)
+    let gridContainer = document.createElement('div');
+    gridContainer.style.display = 'grid';
+    gridContainer.style.width = '100%';
+    gridContainer.style.gridAutoFlow = 'column';
+    gridContainer.style.grid = `${gridWidth}px `.repeat(Math.ceil(filteredQuizzes.length / gridColumn)) + '/' + ` ${gridWidth}px`.repeat(gridColumn);
+
+    let gridCells = [];
+    filteredQuizzes.forEach((quiz, index) => {
+        let cell = document.createElement('div');
+        cell.style.background = `url(${quiz[0]})`;
+        cell.style.backgroundSize = 'cover';
+        cell.style.backgroundPosition = 'center';
+        cell.style.borderTop = (index + 1) <= gridColumn ? borderSize : '0';
+        cell.style.borderLeft = (index + gridColumn) % gridColumn == 0 ? borderSize : '0';
+        cell.style.borderRight = borderSize;
+        cell.style.borderBottom = borderSize;
+        cell.style.borderColor = borderColor;
+        cell.style.borderStyle = 'solid';
+        gridContainer.appendChild(cell);
+
+        let already = document.createElement('div');
+        already.style.background = borderColor;
+        already.style.width = 'max-content';
+        already.innerText = '出題済';
+        if(quiz[1]) cell.appendChild(already);
+    })
+    
+    imagesAreaElem.innerHTML = '';
+    imagesAreaElem.appendChild(gridContainer);
+}
+
+function toggleShowAll() {
+    showAll = !showAll;
+    createImageGrid();
 }
 
 function random(min, max) {
